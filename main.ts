6407021ba734ed1fff78462f1ef06991a0809724
caikinshing <128443746+caikinshing@users.noreply.github.com>
2023-03-27 15:13:28 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,17 +6,11 @@ basic.showLeds(`
     # . . # .
     `)
 basic.forever(function () {
-    if (mbit_小车类.Line_Sensor(mbit_小车类.enPos.LeftState, mbit_小车类.enLineState.White) && mbit_小车类.Line_Sensor(mbit_小车类.enPos.RightState, mbit_小车类.enLineState.White)) {
-        mbit_小车类.CarCtrlSpeed(mbit_小车类.CarState.Car_Run, 60)
-    } else if (mbit_小车类.Line_Sensor(mbit_小车类.enPos.LeftState, mbit_小车类.enLineState.White) && mbit_小车类.Line_Sensor(mbit_小车类.enPos.RightState, mbit_小车类.enLineState.Black)) {
-        mbit_小车类.CarCtrlSpeed(mbit_小车类.CarState.Car_SpinRight, 70)
-    } else if (mbit_小车类.Line_Sensor(mbit_小车类.enPos.LeftState, mbit_小车类.enLineState.White) && mbit_小车类.Line_Sensor(mbit_小车类.enPos.RightState, mbit_小车类.enLineState.Black)) {
-        mbit_小车类.CarCtrlSpeed(mbit_小车类.CarState.Car_SpinLeft, 70)
-    } else {
-        mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
-    }
+    mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Run)
 })
 basic.forever(function () {
-    mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Left)
-    mbit_小车类.CarCtrlSpeed(mbit_小车类.CarState.Car_Run, 2)
+    while (mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE)) {
+        mbit_小车类.CarCtrlSpeed(mbit_小车类.CarState.Car_Stop, 0)
+        mbit_小车类.CarCtrlSpeed(mbit_小车类.CarState.Car_SpinLeft, 180)
+    }
 })
